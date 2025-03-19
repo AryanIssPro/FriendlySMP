4cd7d3e4206b6332c39fc83e4dcb05a56693a71d
Aryan Gupta <aryan0106gupta@gmail.com>
2025-03-19 17:52:32 +0000

    iiii

--- setup.sh
+++ setup.sh
@@ -8,5 +8,10 @@ curl -fsSL https://tailscale.com/install.sh | sh
 sudo tailscaled & 
 sudo tailscale up
 
+curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
+sudo apt update
+sudo apt install playit -y
+
 chmod +x start.sh
 chmod +x sync.sh
